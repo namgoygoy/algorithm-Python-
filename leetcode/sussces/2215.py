f75373107bf70265b63094230f98d6cf284db1e9
juheon <juheon5145@gmail.com>
2026-07-05 12:50:43 +0900

    해시맵, 세트문제

---
+++ leetcode/sussces/2215.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        nums1 = list(set1 - set2)
+        nums2 = list(set2 - set1)
+
+        result = [nums1, nums2]
+        return result
